42ab347e45ab42a032673a63df1497833e379c25
Iulian Gabriel Burciu <iuliangabrielbig@gmail.com>
2024-10-16 16:05:21 +0300

    added middleware folder

--- server/dist/controllers/weights.js
+++ server/dist/controllers/weights.js
@@ -1,5 +1,6 @@
-import { weightsCollection } from "../src/index2.js";
-export const getWeights = async () => {
-    const weights = await weightsCollection.find();
-    return weights;
-};
+export {};
+// import { weightsCollection } from "../src/index.js";
+// export const getWeights = async () => {
+//   const weights = await weightsCollection.find();
+//   return weights;
+// };
--- server/dist/index.js
+++ server/dist/index.js
@@ -1,52 +1,16 @@
-import { ApolloServer } from "@apollo/server"; // preserve-line
-import { startStandaloneServer } from "@apollo/server/standalone"; // preserve-line
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String!
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+import express from "express";
+import { MongoClient } from "mongodb";
+import { createServer } from "http";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+const app = express();
+const server = createServer(app);
+app.use(cors());
+app.use(express.json());
+const client = await MongoClient.connect(process.env.MONGO_URI);
+export const db = client.db("Weights");
+export const weightsCollection = db.collection("weights");
+server.listen(process.env.PORT, () => {
+    console.log(`server running on http://localhost:${process.env.PORT}`);
 });
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-console.log(`🚀  Server ready at: ${url}`);
